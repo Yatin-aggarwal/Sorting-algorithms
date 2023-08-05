e90b41492cdda4ae0d9104f2b6d7ed61beeaf1d3
Yatin_aggarwal <yatin.aggarwal033@gmail.com>
2023-08-05 16:01:17 +0530

    first commit

---
+++ bubble_sort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void print_array (vector<int> &v ){
+  for(int i = 0 ; i <v .size(); i++){
+    cout<< v[i]<<"   ";
+  }
+  cout << endl;
+}
+void input_array(int n ,vector<int> &v ){
+    for(int i = 0 ; i < n ; i++){
+        cout<< i<< " element: " ;
+        int temp ;
+        cin>> temp;
+        v.emplace_back(temp);
+
+    }
+    cout<< endl;
+}
+
+void sort_array(vector<int> &v){
+      for(int i = 1 ; i < v.size();i++){
+        for(int j = 0 ; j < v.size() - i ; j++){
+            if(v[j]> v[j+1] )swap(v[j],v[j+1]);
+        }
+      }
+}
+int main(){
+int n ;
+cout << "vector size: "; 
+cin>> n ; 
+vector<int> v;    
+input_array(n,v);
+print_array(v);
+sort_array(v);
+print_array(v);
+}
